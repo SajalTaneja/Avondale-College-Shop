14829e402cdb22de2f7d49902b9b4818bd4ad45d
ac116940 <ac116940@8CG8163KZB>
2023-03-13 15:20:28 +1300

    Add project files.

---
+++ Avondale College Shop/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Avondale_College_Shop.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
