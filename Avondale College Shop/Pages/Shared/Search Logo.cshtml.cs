041c61e08be53cc4d3f20ab442194ddd9630500c
Sajal Taneja <ac116940@avcol.school.nz>
2023-03-20 15:20:17 +1300

    Added text

---
+++ Avondale College Shop/Pages/Shared/Search Logo.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Avondale_College_Shop.Pages.Shared
+{
+    public class Search_LogoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
