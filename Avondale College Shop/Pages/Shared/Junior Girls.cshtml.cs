fa7ab83551e63e600dafeff44ce6f1786a949d84
Sajal Taneja <ac116940@avcol.school.nz>
2023-03-30 11:09:44 +1300

    added dropdown

---
+++ Avondale College Shop/Pages/Shared/Junior Girls.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Avondale_College_Shop.Pages.Shared
+{
+    public class Junior_GirlsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Avondale College Shop/Pages/Shared/Senior Boys.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Avondale_College_Shop.Pages.Shared
+{
+    public class Senior_BoysModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
