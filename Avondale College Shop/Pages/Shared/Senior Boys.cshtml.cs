59dbe569c3919b2714ee3b980847ddd38683ba84
Sajal Taneja <ac116940@avcol.school.nz>
2023-03-31 10:15:01 +1300

    fixed the privacy page

--- Avondale College Shop/Pages/Shared/Senior Boys.cshtml.cs
+++ Avondale College Shop/Pages/Shared/Senior Boys.cshtml.cs
@@ -5,6 +5,13 @@ namespace Avondale_College_Shop.Pages.Shared
 {
     public class Senior_BoysModel : PageModel
     {
+
+        private readonly ILogger<Senior_BoysModel> _logger;
+
+        public Senior_BoysModel(ILogger<Senior_BoysModel> logger)
+        {
+            _logger = logger;
+        }
         public void OnGet()
         {
         }
