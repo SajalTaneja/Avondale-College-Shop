e022444973d91e2df5f3ddd94878e49a85235e10
Sajal Taneja <ac116940@avcol.school.nz>
2023-06-16 10:10:20 +1200

    fixed the shipment thing

---
+++ Avondale College Shop/Migrations/20230615220626_update.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Avondale_College_Shop.Migrations
+{
+    public partial class update : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ShipOrderTime",
+                table: "Shipment",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "OrderNumber",
+                table: "Shipment",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ShipOrderTime",
+                table: "Shipment",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "OrderNumber",
+                table: "Shipment",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
