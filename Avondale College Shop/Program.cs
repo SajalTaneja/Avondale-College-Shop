8d07e95661d0c45b45446952223484bc9bd4c084
Sajal Taneja <ac116940@avcol.school.nz>
2023-07-24 15:19:55 +1200

    got the project back!!

--- Avondale College Shop/Program.cs
+++ Avondale College Shop/Program.cs
@@ -4,38 +4,38 @@ using Avondale_College_Shop.Areas.Identity.Data;
 
 
 
-            var builder = WebApplication.CreateBuilder(args);
-            
-            builder.Services.AddRazorPages();
+var builder = WebApplication.CreateBuilder(args);
 
-            //var connectionString = builder.Configuration.GetConnectionString("AvondaleDbContextConnection") ?? throw new InvalidOperationException("Connection string 'AvondaleDbContextConnection' not found.");
-            //builder.Services.AddDbContext<AvondaleDbContext>(options =>
-            //    options.UseSqlServer(connectionString));
+builder.Services.AddRazorPages();
 
-            //builder.Services.AddIdentity<AvondaleCollegeShopUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = false)
-            //    .AddRoles<IdentityRole>()
-            //    .AddEntityFrameworkStores<AvondaleDbContext>().AddDefaultTokenProviders();
+var connectionString = builder.Configuration.GetConnectionString("AvondaleDbContextConnection") ?? throw new InvalidOperationException("Connection string 'AvondaleDbContextConnection' not found.");
+builder.Services.AddDbContext<AvondaleDbContext>(options =>
+    options.UseSqlServer(connectionString));
 
-            // Add services to the container.
-           
-            var app = builder.Build();
+builder.Services.AddIdentity<AvondaleCollegeShopUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<AvondaleDbContext>().AddDefaultTokenProviders();
 
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+// Add services to the container.
 
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
+var app = builder.Build();
 
-            app.UseRouting();
-            app.UseAuthentication(); ;
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
 
-            app.UseAuthorization();
+app.UseHttpsRedirection();
+app.UseStaticFiles();
 
-            app.MapRazorPages();
+app.UseRouting();
+app.UseAuthentication(); ;
 
+app.UseAuthorization();
 
+app.MapRazorPages();
+
+app.Run();
